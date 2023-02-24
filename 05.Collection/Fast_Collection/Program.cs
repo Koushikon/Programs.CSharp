d7bace94c0a0e25dd7302ac198fba122e2016ccd
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-02-25 02:45:06 +0530

    Info: Change Arrays dir into Collection

---
+++ 05.Collection/Fast_Collection/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+/**
+ * ? Fastest Collections in C#
+*/
+
+namespace Fast_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * ? HashSet<T> Class:
+            */
+            var hashSet = new HashSet<int>();
+
+            hashSet.Add(71);
+            hashSet.Add(57);
+            hashSet.Add(32);
+            hashSet.Add(57); // Only Take Unique values, Duplicate values is ignored.
+
+            Console.WriteLine("\n= = = = = = HashSet ::");
+            foreach (var item in hashSet)
+            {
+                Console.Write($"Value: {item}, ");
+            }
+            Console.WriteLine($"\nTotal No. of Values: {hashSet.Count}");
+
+            /**
+             * ? Dictionary<TKey, TValue> Class:
+            */
+            var dictSet = new Dictionary<int, string>();
+            dictSet.Add(1, "Hawgarts I");
+            dictSet.Add(2, "Hawgarts II");
+            dictSet.Add(3, "Hawgarts IV");
+            dictSet.Add(4, "Python-5.0");
+
+            Console.WriteLine("\n= = = = = = Dictionary ::");
+            Console.WriteLine($"2nd Dictionary Set: {dictSet[2]}");
+            Console.WriteLine($"3rd Dictionary Set: {dictSet[3]}");
+            Console.WriteLine($"Total No. of Values: {dictSet.Count}");
+
+            /**
+             * ? LinkedList<T> Class:
+            */
+            var linkedList = new LinkedList<string>();
+            linkedList.AddLast("Parallel");
+            linkedList.AddLast("Universe");
+            linkedList.AddLast("Paradox");
+
+            Console.WriteLine("\n= = = = = = LinkedList ::");
+            foreach (var item in linkedList)
+            {
+                Console.Write($"Value: {item}, ");
+            }
+            Console.WriteLine($"\nTotal No. of Values: {linkedList.Count}");
+
+            /**
+             * ? Stack<T> Class:
+            */
+            var stack = new Stack<string>();
+            stack.Push("ONE");
+            stack.Push("TWO");
+            stack.Push("THREE");
+
+            Console.WriteLine("\n= = = = = = Stack ::");
+            foreach (var item in stack)
+            {
+                Console.Write($"Value: {item}, ");
+            }
+            Console.WriteLine($"\nTotal No. of Values: {stack.Count}");
+
+            /**
+             * ? Queue<T> Class:
+            */
+            var queue = new Queue<string>();
+            queue.Enqueue("FOUR");
+            queue.Enqueue("FIVE");
+            queue.Enqueue("SIX");
+
+            Console.WriteLine("\n= = = = = = Queue ::");
+            foreach (var item in queue)
+            {
+                Console.Write($"Value: {item}, ");
+            }
+            Console.WriteLine($"\nTotal No. of Values: {queue.Count}");
+        }
+    }
+}
---
+++ 05.Collection/IEnumerable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iterate through Ienumerable and Show the Data:");
+            Display_As_Ineumerable();
+
+            Console.WriteLine("\nIterate through a List and Show the Data:");
+            Display_As_List();
+        }
+
+        private static void Display_As_Ineumerable()
+        {
+            var names = new List<string> {
+                "sam", "alexia", "simon", "sumanth", "tony", "sam", "amr", "mark", "drew"
+            };
+
+            IEnumerable<string> Result = names.Where(w => w.Length > 5);
+            names[0] = "benjamin";
+
+            foreach (var name in Result)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        private static void Display_As_List()
+        {
+            var names = new List<string> {
+            "sam", "alexia", "simon", "sumanth", "tony", "sam", "amr", "mark", "drew"
+        };
+
+            List<string> Result = names.Where(w => w.Length > 5).ToList();
+            names[0] = "benjamin";
+
+            foreach (var name in Result)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
