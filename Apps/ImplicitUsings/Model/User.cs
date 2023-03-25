d2a18726fc510a6a0e8a1a42f6f39a68f16c1a5e
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-03-25 14:55:23 +0530

    Add: Implicit usings and global usings in .Net new feature

---
+++ Apps/ImplicitUsings/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ImplicitUsings.Model;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
---
+++ Apps/ImplicitUsings/Program.cs
@@ -0,0 +1,14 @@
+﻿// Using Implicit Usings
+// File location: obj/Debug/net6.0/<some_name>.GlobalUsings.g.cs
+
+Console.WriteLine("ImplicitUsings || Global Usings with file \"global.usings.cs\"");
+
+User user = new User()
+{
+    Id = 1,
+    Name = "Test",
+    Email = "Test@gmail.com",
+    Age = 17,
+};
+
+Console.WriteLine($"User: Name= {user.Name}, Email= {user.Email}, Age= {user.Age}");
