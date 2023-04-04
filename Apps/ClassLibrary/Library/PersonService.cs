ba8fe6780a2fab0546f66dee22eb9992425ad1f4
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-04-05 00:12:30 +0530

    Add: Visual Studio Class Library Project

---
+++ Apps/ClassLibrary/Library/PersonService.cs
@@ -0,0 +1,8 @@
+﻿namespace Library;
+
+public class PersonService
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string FullName => $"{FirstName} {LastName}";
+}
---
+++ Apps/ClassLibrary/Matrix/Program.cs
@@ -0,0 +1,16 @@
+﻿using Library;
+
+namespace Matrix;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var personS = new PersonService();
+        
+        personS.FirstName = "Anirban";
+        personS.LastName = "Panja";
+
+        Console.WriteLine(personS.FullName);
+    }
+}
