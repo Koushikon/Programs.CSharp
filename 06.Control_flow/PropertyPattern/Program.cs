767f8960c48951020f0488dc5c45bc3f0096459f
Koushikon <54022602+Koushikon@users.noreply.github.com>
2023-04-05 00:09:41 +0530

    Add: C# Property Pattern matching

---
+++ 06.Control_flow/PropertyPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+
+/**
+ * Title: Extended Property Pattern
+ * Description: C# Property Pattern Code
+ */
+
+namespace PropertyPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var data = new PersonModel();
+
+            /**
+             * * Before C# Version 10
+             */
+            if (data is { FirstName: "Tim", Address: { City: "Kolkata" } })
+            {
+                Console.WriteLine($"Before C# Version 10");
+            }
+
+            /**
+             * * After C# Version 10
+             */
+            if (data is { FirstName: "Tim", Address.City: "Kolkata", Address.ZipCode: "700001" })
+            {
+                Console.WriteLine($"After C# Version 10");
+            }
+        }
+    }
+
+    public class PersonModel
+    {
+        public string FirstName { get; set; } = "Tim";
+        public string LastName { get; set; } = "Corey";
+        public AddressModel Address { get; set; } = new AddressModel();
+    }
+
+    public class AddressModel
+    {
+        public string City { get; set; } = "Kolkata";
+        public string State { get; set; } = "West Bengal";
+        public string ZipCode { get; set; } = "700001";
+    }
+}
